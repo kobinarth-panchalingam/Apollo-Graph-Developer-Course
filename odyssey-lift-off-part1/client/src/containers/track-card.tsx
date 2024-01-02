fbb4b45cef3dfc735ed13c7e8a240d3ad3e2ac8b
Kobinarth Panchalingam <p.kobinarth@gtngroup.com>
2024-01-02 10:18:58 +0530

    add codegen type genrator

--- odyssey-lift-off-part1/client/src/containers/track-card.tsx
+++ odyssey-lift-off-part1/client/src/containers/track-card.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import styled from "@emotion/styled";
 import { colors, mq } from "../styles";
 import { humanReadableTimeFromSeconds } from "../utils/helpers";
+import type { Track } from "../__generated__/graphql";
 
 /**
  * Track Card component renders basic info in a card format
  * for each track populating the tracks grid homepage.
  */
-const TrackCard: React.FC<{ track: any }> = ({ track }) => {
+const TrackCard: React.FC<{ track: Track }> = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount } = track;
 
   return (
@@ -23,8 +24,7 @@ const TrackCard: React.FC<{ track: any }> = ({ track }) => {
             <AuthorAndTrack>
               <AuthorName>{author.name}</AuthorName>
               <TrackLength>
-                {modulesCount} modules -{" "}
-                {humanReadableTimeFromSeconds(length || 0)}
+                {modulesCount} modules - {humanReadableTimeFromSeconds(length || 0)}
               </TrackLength>
             </AuthorAndTrack>
           </CardFooter>
